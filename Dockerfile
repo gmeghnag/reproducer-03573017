ba639e36401eee3949ef79b9cd35b2467412a39b
gmeghnag <gmeghnag@redhat.com>
2023-07-27 18:36:42 +0200

    modified code

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ RUN mkdir /app
 WORKDIR /app
 COPY app.py app.py
 EXPOSE 8000
-CMD uvicorn app:app --host 0.0.0.0 --port 8000
+CMD python3 app.py
--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_root()-> str:
+    return "Fast api is up and running. Use swagger UI at /docs."
 
 
-@app.post("/hello")
-def world():
-    return {"Post": "World"}
+@app.post("/model/dollyv2/instruct_generate")
+async def test():
+    return {"post": "output"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
